bb5939103ae233e056cb13b5f1dbc73d48fdb305
Gregoire BURNET <44721323+Webgburnet@users.noreply.github.com>
2024-04-08 10:33:20 +0200

    Restaurer '08/04/2024 09:50:25 Mise à jour main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,1 +1,33 @@
-
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "A") {
+    	
+    } else if (receivedString == "B") {
+    	
+    } else if (receivedString == "Z") {
+    	
+    } else if (receivedString == "Rouge") {
+        cuteBot.colorLight(cuteBot.RGBLights.ALL, 0xff0000)
+    } else if (receivedString == "Vert") {
+        cuteBot.colorLight(cuteBot.RGBLights.ALL, 0x00ff00)
+    } else if (receivedString == "Bleu") {
+        cuteBot.colorLight(cuteBot.RGBLights.ALL, 0x0000ff)
+    } else if (receivedString == "Jaune") {
+        cuteBot.colorLight(cuteBot.RGBLights.ALL, 0xffff00)
+    } else {
+    	
+    }
+})
+radio.onReceivedValue(function (name, value) {
+    if (name == "x") {
+        x = value
+    }
+    if (name == "y") {
+        y = value
+    }
+})
+let y = 0
+let x = 0
+radio.setGroup(255)
+basic.forever(function () {
+    cuteBot.motors(y + x, y - x)
+})
